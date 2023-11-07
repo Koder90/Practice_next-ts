be27ad370181f752f271fe34d15dafbcc3e282c0
Koder90 <jerko.zlopasa@hotmail.com>
2023-11-07 12:00:58 +0000

    change background

--- practice/app/components/navbar/navbar.tsx
+++ practice/app/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar (){
     return (
         <nav className={styles.nav}>
             <h1 className={styles.item}>Home</h1>
-            <Link href="/about"><h1 className={styles.item}>About</h1></Link>
+            <Link href="/about" style={{textDecoration:"none", color: "black"}}><h1 className={styles.item}>About</h1></Link>
             <h1 className={styles.item}>Login</h1>
         </nav>
     )
--- practice/app/layout.tsx
+++ practice/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import backgroundImage from "../public/background.jpg"
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{
+        // use the src property of the image object
+        backgroundImage: `url(${backgroundImage.src})`,
+        // other styles
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
         {children}</body>
     </html>
   )
