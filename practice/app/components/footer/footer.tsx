720567273e0a1d8a8b3b049df03fb33b1813cc87
Koder90 <jerko.zlopasa@hotmail.com>
2023-11-08 12:51:26 +0000

    Edited footer with social links

--- practice/app/components/footer/footer.tsx
+++ practice/app/components/footer/footer.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import styles from './style.module.css'
+import React from "react";
+import styles from "./style.module.css";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { BiLogoTwitter } from "react-icons/bi";
+import { BiLogoFacebook } from "react-icons/bi";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>Footer</div>
-  )
+    <div className={styles.footer}>
+      <h3 className="ml-10">2023&copy;Koder90</h3>
+      <ul className="flex justify-center mr-10 mt-2">
+        <li className="mr-5">
+          <Link href="https://www.facebook.com/jerko.zlopasa">
+            <BiLogoFacebook size={30} />
+          </Link>
+        </li>
+        <li className="mr-5">
+          <Link href="https://www.linkedin.com/in/jerko-zlopasa/">
+            <BiLogoLinkedin size={30} />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://twitter.com/">
+            <BiLogoTwitter size={30} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
