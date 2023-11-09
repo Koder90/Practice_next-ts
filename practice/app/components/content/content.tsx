c6880c46c8fc16b912b8164fed083a09f4eca9bc
Koder90 <jerko.zlopasa@hotmail.com>
2023-11-09 13:03:23 +0000

    added initial skills styling

--- practice/app/components/content/content.tsx
+++ practice/app/components/content/content.tsx
@@ -4,7 +4,8 @@ import styles from "./styles.module.css";
 export default function Content() {
   return (
     <div className={styles.content}>
-      <h1 className={styles.item}>Content</h1>
+      <h1 className="text-white text-2xl">Hi, my name is Jerko</h1>
+      <p className="text-white">I am a frontend web developer/UI designer</p>
     </div>
   );
 }
--- practice/app/components/skills/skills.tsx
+++ practice/app/components/skills/skills.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css'
-import { BiLogoReact } from 'react-icons/bi'
+import { BiLogoGit, BiLogoReact } from 'react-icons/bi'
+import { BiLogoNodejs } from 'react-icons/bi'
 
 export default function Skills() {
   return (
     <>
         <div>
             <h3 className={styles.title}>My skills</h3>
-            <div className={styles.grid_container}><BiLogoReact /></div>
+            <div className={styles.grid_container}>
+              <BiLogoReact size={50} />
+              <BiLogoNodejs size={50} />
+              <BiLogoGit size={50} />
+              </div>
             
         </div>
     </>
