1b8ff9d4eadaff9390e67795cf759a02b90e8141
Koder90 <jerko.zlopasa@hotmail.com>
2023-11-10 12:38:54 +0000

    Styled about page and added projects page

--- practice/app/about/page.tsx
+++ practice/app/about/page.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const About = () => {
     return (
-        <>
-        <h1>
-            about
+        <div className='relative w-322 h-322 top-40'>
+        <h1 className='text-white text-2xl underline mb-4'>
+            About me
         </h1>
-        <p>Here we practice Nextjs and typescript</p>
-    </>
+        <p className='text-white'>I am a frontend developer focused on mastering new exciting technology called Next.js. </p><p className='text-white underline underline-offset-8 mb-8'>I am a social worker who has found passion in web development and looking for work.</p>
+        <p className ='text-white'>I have gathered some experience freelancing so I can join in on projects on a junior level</p>
+    </div>
     )
 }
 
--- practice/app/components/navbar/navbar.tsx
+++ practice/app/components/navbar/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar (){
         <nav className={styles.nav}>
             <h1 className={styles.item}>Home</h1>
             <Link href="/about" style={{textDecoration:"none", color: "black"}}><h1 className={styles.item}>About</h1></Link>
-            <h1 className={styles.item}>Login</h1>
+            <Link href="/projects"><h1 className={styles.item}>Projects</h1></Link>
         </nav>
     )
 }
