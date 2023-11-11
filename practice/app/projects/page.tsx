284f42d3b46bc1502c0e3af399f9c17e1c0d2dde
Koder90 <Koder90>
2023-11-11 13:46:00 +0100

    working on projects page

--- practice/app/projects/page.tsx
+++ practice/app/projects/page.tsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import Image from 'next/image'
+import gameFinderImage from '../../public/gamefinderimg.png'
 
 export default function Projects() {
   return (
-    <div className='text-white'>Here are my projects</div>
+    <div>
+      <div className='text-white'>Here are my projects</div>
+      <div className='grid grid-cols-3 gap-4'>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+<Image 
+src={gameFinderImage}
+alt='gamefinder'
+height={300}
+width={300}
+/>
+      </div>
+    </div>
   )
 }
